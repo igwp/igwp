cc54f4305b42b63becd372d73aafff493de32adc
AG Stephan <godtheresnonamesleft@gmail.com>
2016-09-22 20:47:58 -0700

    formatted overlay slightly better, split out styles into a stylesheet

--- InGameProbabilities/InGameProbabilitiesApp/main.js
+++ InGameProbabilities/InGameProbabilitiesApp/main.js
@@ -23,6 +23,12 @@ plugin.initialize(function(status) {
     }
     document.querySelector('#messages').appendChild(obj)
   });
+
+    overwolf.windows.getCurrentWindow(function(result) {
+        overwolf.windows.changePosition(result.window.id, 0, 16, function()   {
+            overwolf.windows.changeSize(result.window.id, 1920, 400, null);
+        });
+    });
    
   //document.querySelector('#title').innerText =
   //  "Plugin " + plugin.get()._PluginName_ + " was loaded!";
